41d3c54664df947fcdd12e6b286d4021c726d3d2
suifengsigan <1075087594@qq.com>
2018-08-03 16:58:11 +0800

    优化

---
+++ CMM/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CMM
+{
+    public static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Show(args);
+        }
+
+        static void Show(string[] args)
+        {
+            System.Windows.Forms.MessageBox.Show("123");
+        }
+
+     
+    }
+}
