946510cff220cfc4d922d708ce1f7ef07c28efdb
suifengsigan <1075087594@qq.com>
2018-08-20 11:31:43 +0800

    1、优化

--- CMM/CMMBusiness.cs
+++ CMM/CMMBusiness.cs
@@ -64,7 +64,7 @@ namespace CMM
             var faces = elec.ElecHeadFaces;
             foreach (var face in faces)
             {
-                var positions = GetFacePoints(face);
+                var positions = SnapHelper.GetFacePoints(face);
                 var faceMidPoint = face.Position((face.BoxUV.MaxU + face.BoxUV.MinU) / 2, (face.BoxUV.MaxV + face.BoxUV.MinV) / 2);
                 var ps = positions.OrderBy(u => Snap.Position.Distance(faceMidPoint, u)).ToList();
                 for (var i = 0; i < LoopVarValue; i++)
@@ -91,7 +91,7 @@ namespace CMM
         {
             var result = new List<PointData>();
             var face = elec.BaseFace;
-            var positions = GetFacePoints(face);
+            var positions = SnapHelper.GetFacePoints(face);
             var vector = face.GetFaceDirection();
             //边界点
             var p1 = face.Position(face.BoxUV.MinU, face.BoxUV.MinV);
@@ -174,27 +174,7 @@ namespace CMM
         {
             //创建射线检测
         }
-
-        /// <summary>
-        /// 干涉检查（1 -> there is interference  2 -> no interference  3 -> touching, that is coincident faces）
-        /// </summary>
-        static bool CheckInterference(NXOpen.Tag targetBody,NXOpen.Tag toolBody)
-        {
-            var result = true;
-            var ufSession = NXOpen.UF.UFSession.GetUFSession();
-            int[] r = new int[] { 0 };
-            ufSession.Modl.CheckInterference(targetBody, 1, new NXOpen.Tag[] { toolBody }, r);
-            result = r[0] != 3;
-            return result;
-        }
-
-        /// <summary>
-        /// 点是否在面上
-        /// </summary>
-        static bool IsPointAtFace(Snap.NX.Face face, Snap.Position p)
-        {
-            return Snap.Compute.Distance(p, face) < SnapEx.Helper.Tolerance;
-        }
+        
 
         /// <summary>
         /// 是否小于探针半径
@@ -210,78 +190,6 @@ namespace CMM
                 }
             }
             return false;
-        }
-
-        /// <summary>
-        /// 获取面上所有的测量点
-        /// </summary>
-        static List<Snap.Position> GetFacePoints(Snap.NX.Face face, double max_facet_size = 1)
-        {
-            var mark = Snap.Globals.SetUndoMark(Globals.MarkVisibility.Visible, "GetFacePoints");
-            var positions = new List<Snap.Position>();
-            try
-            {
-                #region old code
-                var parameters = new UFFacet.Parameters();
-
-                var ufSession = NXOpen.UF.UFSession.GetUFSession();
-                var facet = ufSession.Facet;
-                facet.AskDefaultParameters(out parameters);
-                parameters.max_facet_edges = 3;
-                parameters.specify_max_facet_size = true;
-                parameters.max_facet_size = 1;
-
-                NXOpen.Tag facet_model = NXOpen.Tag.Null;
-                facet.FacetSolid(face.NXOpenTag, ref parameters, out facet_model);
-
-                if (facet_model == NXOpen.Tag.Null) return positions;
-                NXOpen.Tag solid = NXOpen.Tag.Null;
-                facet.AskSolidOfModel(facet_model, out solid);
-                if (solid != face.NXOpenTag) return positions;
-
-                int facet_id = NXOpen.UF.UFConstants.UF_FACET_NULL_FACET_ID;
-                bool isWhile = true;
-                while (isWhile)
-                {
-                    facet.CycleFacets(facet_model, ref facet_id);
-                    if (facet_id != NXOpen.UF.UFConstants.UF_FACET_NULL_FACET_ID)
-                    {
-                        int num_vertices = 0;
-                        facet.AskNumVertsInFacet(facet_model, facet_id, out num_vertices);
-                        if (num_vertices == 3)
-                        {
-                            var vertices = new double[num_vertices, 3];
-                            facet.AskVerticesOfFacet(facet_model, facet_id, out num_vertices, vertices);
-                            for (int i = 0; i < num_vertices; i++)
-                            {
-                                int pt_status = 0;
-                                var position = new Snap.Position(vertices[i, 0], vertices[i, 1], vertices[i, 2]);
-                                ufSession.Modl.AskPointContainment(position.Array, face.NXOpenTag, out pt_status);
-                                if (0x1 == pt_status || 0x3 == pt_status)
-                                {
-                                    positions.Add(position);
-                                }
-                            }
-                        }
-                    }
-                    else
-                    {
-                        isWhile = false;
-                    }
-                }
-
-                ufSession.Obj.DeleteObject(facet_model);
-                positions = positions.Distinct().ToList();
-                #endregion
-               
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                Snap.Globals.UndoToMark(mark, null);
-            }
-
-            return positions;
         }
     }
 }
---
+++ CMM/SnapHelper.cs
@@ -0,0 +1,142 @@
+﻿using NXOpen.UF;
+using Snap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CMM
+{
+    public class SnapHelper
+    {
+        public static Snap.NX.Body ImportPart(string fileName)
+        {
+            Snap.NX.Body result = null;
+            NXOpen.UF.ImportPartModes modes = new NXOpen.UF.ImportPartModes();
+            //坐标系
+            double[] dest_csys = new double[6];
+            //基准点
+            double[] dest_point = new double[3];
+
+            dest_csys[0] = 1; //坐标系X轴的矢量
+            dest_csys[1] = 0;
+            dest_csys[2] = 0;
+            dest_csys[3] = 0; //坐标系Y轴的矢量
+            dest_csys[4] = 1;
+            dest_csys[5] = 0;
+            dest_point[0] = 0.0; //基准点【导入到点坐标】
+            dest_point[1] = 0.0;
+            dest_point[2] = 0.0;
+
+            //导入对象比例缩放
+            double scale = 1.0;
+            NXOpen.Tag group;
+            NXOpen.UF.UFSession.GetUFSession().Part.Import(fileName, ref modes, dest_csys, dest_point, scale, out group);
+
+            foreach (var m in Snap.Globals.WorkPart.Bodies)
+            {
+                var axisFace = m.Faces.FirstOrDefault(u => u.Name == SnapEx.ConstString.CMM_INSPECTION_AXISPOINT);
+                if (axisFace != null)
+                {
+                    result = m;
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 点是否在面上
+        /// </summary>
+        public static bool IsPointAtFace(Snap.NX.Face face, Snap.Position p)
+        {
+            return Snap.Compute.Distance(p, face) < SnapEx.Helper.Tolerance;
+        }
+
+        /// <summary>
+        /// 干涉检查（1 -> there is interference  2 -> no interference  3 -> touching, that is coincident faces）
+        /// </summary>
+        public static bool CheckInterference(NXOpen.Tag targetBody, NXOpen.Tag toolBody)
+        {
+            var result = true;
+            var ufSession = NXOpen.UF.UFSession.GetUFSession();
+            int[] r = new int[] { 0 };
+            ufSession.Modl.CheckInterference(targetBody, 1, new NXOpen.Tag[] { toolBody }, r);
+            result = r[0] != 3;
+            return result;
+        }
+
+        /// <summary>
+        /// 获取面上所有的测量点
+        /// </summary>
+        public static List<Snap.Position> GetFacePoints(Snap.NX.Face face, double max_facet_size = 1)
+        {
+            var mark = Snap.Globals.SetUndoMark(Globals.MarkVisibility.Visible, "GetFacePoints");
+            var positions = new List<Snap.Position>();
+            try
+            {
+                #region old code
+                var parameters = new UFFacet.Parameters();
+
+                var ufSession = NXOpen.UF.UFSession.GetUFSession();
+                var facet = ufSession.Facet;
+                facet.AskDefaultParameters(out parameters);
+                parameters.max_facet_edges = 3;
+                parameters.specify_max_facet_size = true;
+                parameters.max_facet_size = 1;
+
+                NXOpen.Tag facet_model = NXOpen.Tag.Null;
+                facet.FacetSolid(face.NXOpenTag, ref parameters, out facet_model);
+
+                if (facet_model == NXOpen.Tag.Null) return positions;
+                NXOpen.Tag solid = NXOpen.Tag.Null;
+                facet.AskSolidOfModel(facet_model, out solid);
+                if (solid != face.NXOpenTag) return positions;
+
+                int facet_id = NXOpen.UF.UFConstants.UF_FACET_NULL_FACET_ID;
+                bool isWhile = true;
+                while (isWhile)
+                {
+                    facet.CycleFacets(facet_model, ref facet_id);
+                    if (facet_id != NXOpen.UF.UFConstants.UF_FACET_NULL_FACET_ID)
+                    {
+                        int num_vertices = 0;
+                        facet.AskNumVertsInFacet(facet_model, facet_id, out num_vertices);
+                        if (num_vertices == 3)
+                        {
+                            var vertices = new double[num_vertices, 3];
+                            facet.AskVerticesOfFacet(facet_model, facet_id, out num_vertices, vertices);
+                            for (int i = 0; i < num_vertices; i++)
+                            {
+                                int pt_status = 0;
+                                var position = new Snap.Position(vertices[i, 0], vertices[i, 1], vertices[i, 2]);
+                                ufSession.Modl.AskPointContainment(position.Array, face.NXOpenTag, out pt_status);
+                                if (0x1 == pt_status || 0x3 == pt_status)
+                                {
+                                    positions.Add(position);
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        isWhile = false;
+                    }
+                }
+
+                ufSession.Obj.DeleteObject(facet_model);
+                positions = positions.Distinct().ToList();
+                #endregion
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Snap.Globals.UndoToMark(mark, null);
+            }
+
+            return positions;
+        }
+    }
+}
