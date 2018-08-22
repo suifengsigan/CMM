4a0cd73dcc6a4d3d3feb543e0965f0f6b627e7e1
suifengsigan <1075087594@qq.com>
2018-08-22 17:35:47 +0800

    优化

---
+++ EactTool/FileHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EactTool
+{
+    /// <summary>
+    /// 文件帮助类
+    /// </summary>
+    public class FileHelper
+    {
+        const string EACTERROR = "EACTERROR";
+        public string FindFile(string path)
+        {
+            return string.Empty;
+        }
+    }
+}
