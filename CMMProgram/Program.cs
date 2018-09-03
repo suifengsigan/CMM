f09bd7e8415c272e181d496e3006ade9c69d5a45
suifengsigan <1075087594@qq.com>
2018-09-03 11:29:54 +0800

    优化

--- CMMProgram/Program.cs
+++ CMMProgram/Program.cs
@@ -17,63 +17,9 @@ namespace CMMProgram
         static void Main()
         {
             System.IO.Directory.SetCurrentDirectory(UGBASEDIRUGII);
-            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new s());
-        }
-
-        static string PathCombine(params string[] str)
-        {
-            var result = string.Empty;
-            str.ToList().ForEach(u => {
-                result = Path.Combine(result, u);
-            });
-            return result;
-        }
-
-        private static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
-        {
-            var assemblyName = new AssemblyName(args.Name);
-            if (assemblyName.Name.Contains("NXOpen"))
-            {
-                var dir = UGBASEDIRUGII;
-                var info = new System.IO.DirectoryInfo(dir);
-                var UGII_BASE_DIR = info.Parent.FullName;
-
-                var UGMANAGEDPATH = PathCombine(dir, "managed", assemblyName.Name + ".dll");
-                if (!File.Exists(UGMANAGEDPATH))
-                {
-                    UGMANAGEDPATH = PathCombine(UGII_BASE_DIR, "NXBIN", "managed", assemblyName.Name + ".dll");
-                }
-                if (File.Exists(UGMANAGEDPATH))
-                {
-                    return Assembly.LoadFile(UGMANAGEDPATH);
-                }
-            }
-            else if (assemblyName.Name.Contains("PHSnap"))
-            {
-                Console.WriteLine("Resolving...");
-                var assemblbyName = args.Name.Split(',').FirstOrDefault();
-                var version = "UG9.0";
-                var path = string.Format("{0}\\SnapDll\\{1}", PathCombine(UGBASEDIRUGII, "CMMProg", "Application"), version);
-                var file = System.IO.Directory.GetFiles(path).FirstOrDefault(u => u == string.Format("{0}.dll", System.IO.Path.Combine(path, assemblbyName)));
-                return Assembly.LoadFile(file);
-            }
-            else
-            {
-                var fileName = PathCombine(UGBASEDIRUGII, "CMMProg", "Application", assemblyName.Name + ".dll");
-                if (File.Exists(fileName))
-                {
-                    return Assembly.LoadFile(fileName);
-                }
-                fileName = PathCombine(UGBASEDIRUGII, "CMMProg", "CSharpEx", assemblyName.Name + ".dll");
-                if (File.Exists(fileName))
-                {
-                    return Assembly.LoadFile(fileName);
-                }
-            }
-            return null;
         }
     }
 }
