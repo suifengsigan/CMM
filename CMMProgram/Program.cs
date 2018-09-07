576f52007e4699d653db99693712ca50f889d425
suifengsigan <1075087594@qq.com>
2018-09-07 09:27:14 +0800

    优化

--- CMMProgram/Program.cs
+++ CMMProgram/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace CMMProgram
@@ -16,6 +17,18 @@ namespace CMMProgram
         [STAThread]
         static void Main()
         {
+            #region 系统只运行一次
+
+            bool bCreatedNew;
+            Mutex ltt = new Mutex(false, Path.GetFileNameWithoutExtension(Application.ExecutablePath), out bCreatedNew);
+            if (!bCreatedNew)
+            {
+                MessageBox.Show("已有程序运行中！", "系统提示", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                return;
+            }
+
+            #endregion
+
             System.IO.Directory.SetCurrentDirectory(UGBASEDIRUGII);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
