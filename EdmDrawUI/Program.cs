9b0c4ada68195069985287b1fec10e3e7727159e
suifengsigan <1075087594@qq.com>
2018-10-09 14:41:00 +0800

    优化出图

---
+++ EdmDrawUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EdmDrawUIEx2
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            AssemblyLoader.Entry.InitAssembly();
+            EdmDraw.Upload.Main();
+        }
+
+        public static int GetUnloadOption(string arg)
+        {
+            //return System.Convert.ToInt32(Session.LibraryUnloadOption.Explicitly);
+            return System.Convert.ToInt32(1);
+            // return System.Convert.ToInt32(Session.LibraryUnloadOption.AtTermination);
+        }
+    }
+}
