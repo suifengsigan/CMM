0d22f21078e3ce5e2bbb69c006d3fffb3eb34777
suifengsigan <1075087594@qq.com>
2018-10-16 16:27:22 +0800

    优化

---
+++ EdmDraw/UCEdmConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CMMTool
+{
+    public partial class UCEdmConfig : UserControl
+    {
+        public UCEdmConfig()
+        {
+            InitializeComponent();
+        }
+    }
+}
