1fd64d9de91f9279900a09a57dd94e529de37866
suifengsigan <1075087594@qq.com>
2018-10-29 15:22:07 +0800

    优化

---
+++ AutoCAMUI/Upload.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AutoCAMUI
+{
+    public static class Upload
+    {
+        public static void Main()
+        {
+
+        }
+        public static int GetUnloadOption(string arg)
+        {
+            //return System.Convert.ToInt32(Session.LibraryUnloadOption.Explicitly);
+            return System.Convert.ToInt32(1);
+            // return System.Convert.ToInt32(Session.LibraryUnloadOption.AtTermination);
+        }
+    }
+}
