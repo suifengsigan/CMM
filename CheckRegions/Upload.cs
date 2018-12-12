76bc71801ac59848e12412aa884ca0132f7e8a54
suifengsigan <1075087594@qq.com>
2018-12-12 17:47:29 +0800

    优化

---
+++ CheckRegions/Upload.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CheckRegions
+{
+    public class Upload
+    {
+        public static void Main()
+        {
+            AssemblyLoader.Entry.InitAssembly();
+            Show();
+        }
+
+        private static void Show()
+        {
+            CheckRegionsUI theCheckRegions = null;
+            try
+            {
+                theCheckRegions = new CheckRegionsUI();
+                theCheckRegions.Show();
+            }
+            catch (Exception ex)
+            {
+                NXOpen.UI.GetUI().NXMessageBox.Show("Block Styler", NXOpen.NXMessageBox.DialogType.Error, ex.ToString());
+            }
+            finally
+            {
+                if (theCheckRegions != null)
+                    theCheckRegions.Dispose();
+                theCheckRegions = null;
+            }
+        }
+    }
+}
