1cd813507fba63a23f941c525a1520e5b5509bb9
suifengsigan <1075087594@qq.com>
2018-12-27 14:34:35 +0800

    优化

---
+++ AutoCAMUI/Oper/WsqAutoCAM_CONTOUR_AREA_Oper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AutoCAMUI
+{
+    public class WsqAutoCAM_CONTOUR_AREA_Oper:CAMOper
+    {
+        public WsqAutoCAM_CONTOUR_AREA_Oper()
+        {
+            AUTOCAM_TYPE = "WsqAutoCAM";
+            AUTOCAM_SUBTYPE = "CONTOUR_AREA";
+        }
+    }
+}
---
+++ AutoCAMUI/Oper/WsqAutoCAM_CONTOUR_TEXT_Oper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AutoCAMUI
+{
+    /// <summary>
+    /// 刻字
+    /// </summary>
+    public class WsqAutoCAM_CONTOUR_TEXT_Oper:CAMOper
+    {
+        public WsqAutoCAM_CONTOUR_TEXT_Oper()
+        {
+            AUTOCAM_TYPE = "WsqAutoCAM";
+            AUTOCAM_SUBTYPE = "CONTOUR_TEXT";
+        }
+    }
+}
