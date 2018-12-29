5fb1d0ad52dccd0fe04170e56807f46023ddc310
suifengsigan <1075087594@qq.com>
2018-12-29 11:24:06 +0800

    dd

---
+++ AutoCAMUI/Oper/WsqAutoCAM_FACE_MILLING_TOP_Oper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AutoCAMUI
+{
+    /// <summary>
+    /// 杀顶
+    /// </summary>
+    public class WsqAutoCAM_FACE_MILLING_TOP_Oper: WsqAutoCAM_Oper
+    {
+        public WsqAutoCAM_FACE_MILLING_TOP_Oper()
+        {
+            AUTOCAM_SUBTYPE = "FACE_MILLING_TOP";
+        }
+    }
+}
---
+++ AutoCAMUI/Oper/WsqAutoCAM_Oper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AutoCAMUI
+{
+    public class WsqAutoCAM_Oper:CAMOper
+    {
+        public WsqAutoCAM_Oper()
+        {
+            AUTOCAM_TYPE = "WsqAutoCAM";
+        }
+    }
+}
