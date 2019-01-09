a9e73dfbb7665a0f070bc79ca5ce914dfe85c42b
suifengsigan <1075087594@qq.com>
2019-01-09 09:26:28 +0800

    优化

---
+++ EACT_Start/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EACT_Start
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            txtUgPath.ReadOnly = true;
+            var baseDir = System.Configuration.ConfigurationManager.AppSettings["UGII_BASE_DIR"];
+            if (!string.IsNullOrEmpty(GetUgrafPath(baseDir)))
+            {
+                txtUgPath.Text = baseDir;
+            }
+            else
+            {
+                var ugiibaseDir = System.Environment.GetEnvironmentVariable("UGII_BASE_DIR");
+                if (!string.IsNullOrEmpty(GetUgrafPath(ugiibaseDir)))
+                {
+                    txtUgPath.Text = baseDir;
+                }
+            }
+            txtUgPath.TextChanged += TxtUgPath_TextChanged;
+        }
+
+        private void TxtUgPath_TextChanged(object sender, EventArgs e)
+        {
+            var regUGPath = GetRegUGPath(txtUgPath.Text);
+            if (string.IsNullOrEmpty(txtUgPath.Text) || string.IsNullOrEmpty(regUGPath))
+            {
+                return;
+            }
+            var dirInfo = new System.IO.DirectoryInfo(System.IO.Path.GetDirectoryName(regUGPath));
+            System.Configuration.ConfigurationManager.AppSettings["UGII_BASE_DIR"] = dirInfo.Parent.FullName;
+            System.Configuration.ConfigurationManager.AppSettings["UGII_ROOT_DIR"] = dirInfo.FullName;
+        }
+
+        private void DispMsg(string strMsg)
+        {
+            Action action = () => { DispMsg(strMsg, listBox1); };
+
+            var box = listBox1;
+            if (box.InvokeRequired == false)
+            {
+                action();
+            }
+            else
+            {
+                box.Invoke(action);
+            }
+        }
+
+        private void DispMsg(string strMsg, ListBox box)
+        {
+            Action action = () => {
+                box.Items.Insert(0, string.Format("{0}:{1}", DateTime.Now.ToString(), strMsg));
+                var maxItem = 1000;
+                if (box.Items.Count > maxItem)
+                {
+                    box.Items.RemoveAt(maxItem);
+                }
+            };
+            if (box.InvokeRequired == false)
+            {
+                action();
+            }
+            else
+            {
+                box.Invoke(action);
+            }
+        }
+
+        private string GetRegUGPath(string UGII_BASE_DIR)
+        {
+            var result = string.Empty;
+            var dir = System.IO.Path.GetDirectoryName(GetUgrafPath(UGII_BASE_DIR));
+            var path = System.IO.Path.Combine(dir, "SignDotNet.exe");
+            if (System.IO.File.Exists(path))
+            {
+                return path;
+            }
+
+            path = System.IO.Path.Combine(dir, "SignLibrary.exe");
+            if (System.IO.File.Exists(path))
+            {
+                return path;
+            }
+            return result;
+        }
+
+        private string GetUgrafPath(string UGII_BASE_DIR)
+        {
+            var result = string.Empty;
+            var path = System.IO.Path.Combine(UGII_BASE_DIR, "UGII", "ugraf.exe");
+            if (System.IO.File.Exists(path))
+            {
+                return path;
+            }
+
+            path = System.IO.Path.Combine(UGII_BASE_DIR, "NXBIN", "ugraf.exe");
+            if (System.IO.File.Exists(path))
+            {
+                return path;
+            }
+
+            path = System.IO.Path.Combine(UGII_BASE_DIR, "ugraf.exe");
+            if (System.IO.File.Exists(path))
+            {
+                return path;
+            }
+            return result;
+        }
+
+        private void btnStartUG_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtUgPath.Text))
+            {
+                System.Windows.Forms.MessageBox.Show("路径无法识别，请重新选择！");
+                return;
+            }
+
+            var startupPath = Application.StartupPath;
+
+            Helper.ExecBatCommand(p => {
+                p(string.Format("set UGII_USER_DIR={0}", startupPath));
+                p(string.Format("set UGII_MOLDDM_DIR={0}", startupPath));
+                p(string.Format("set UGII_EACT_DIR={0}", startupPath));
+                p(string.Format("set UGII_BITMAP_PATH={0}\\Application\\EActBitmap", startupPath));
+                p("set UGII_MODL_DEMO=1");
+                p("set UGII_DISPLAY_DEBUG=1");
+                p("set PRINT_DIALOG_BITMAP_NAMES=1");
+                p(string.Format("set UPOINT_DEVELOP_DIR={0}", startupPath));
+                p(string.Format("set UGII_CAM_POST_DIR={0}\\MACH\\resource\\postprocessor\\", startupPath));
+                p("set UGII_LANG=simpl_chinese");
+                p(string.Format("\"{0}\"", GetUgrafPath(txtUgPath.Text)));
+                p("exit 0");
+            },null,false);
+        }
+
+        private void btnSelUG_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog f = new FolderBrowserDialog();
+            f.Description = "请选择UG路径";
+            if (f.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                var path = f.SelectedPath;
+                if (string.IsNullOrEmpty(GetUgrafPath(path)))
+                {
+                    System.Windows.Forms.MessageBox.Show("路径无法识别，请重新选择！");
+                }
+                else
+                {
+                    txtUgPath.Text = path;
+                }
+            }
+        }
+
+        private void btnRegUG_Click(object sender, EventArgs e)
+        {
+            var regUGPath = GetRegUGPath(txtUgPath.Text);
+            if (string.IsNullOrEmpty(txtUgPath.Text) || string.IsNullOrEmpty(regUGPath))
+            {
+                System.Windows.Forms.MessageBox.Show("路径无法识别，请重新选择！");
+                return;
+            }
+            var EACT_UG_SIGNDOTNET = regUGPath;
+            var EACT_UG_USER_APPLICATION = System.IO.Path.Combine(Application.StartupPath, "Application");
+            var list=Helper.ExecBatCommand(p => {
+                p(string.Format("\"{0}\"  {1}\\EactConfig.dll", EACT_UG_SIGNDOTNET, EACT_UG_USER_APPLICATION));
+                p(string.Format("\"{0}\"  {1}\\EactBom.dll", EACT_UG_SIGNDOTNET, EACT_UG_USER_APPLICATION));
+                p(string.Format("\"{0}\"  {1}\\SetProperty.dll", EACT_UG_SIGNDOTNET, EACT_UG_USER_APPLICATION));
+                p(string.Format("\"{0}\"  {1}\\EactTool.dll", EACT_UG_SIGNDOTNET, EACT_UG_USER_APPLICATION));
+                p(string.Format("\"{0}\"  {1}\\CSharpProxy.dll", EACT_UG_SIGNDOTNET, EACT_UG_USER_APPLICATION));
+                p(string.Format("\"{0}\"  {1}\\CMM.dll", EACT_UG_SIGNDOTNET, EACT_UG_USER_APPLICATION));
+                p(string.Format("\"{0}\"  {1}\\AutoPrtTool.dll", EACT_UG_SIGNDOTNET, EACT_UG_USER_APPLICATION));
+                p(string.Format("\"{0}\"  {1}\\CMMManual.dll", EACT_UG_SIGNDOTNET, EACT_UG_USER_APPLICATION));
+                p(string.Format("\"{0}\"  {1}\\CMMTool.dll", EACT_UG_SIGNDOTNET, EACT_UG_USER_APPLICATION));
+                p("exit 0");
+            });
+
+            list.ForEach(u => {
+                DispMsg(u);
+            });
+        }
+
+        private void btnGetPoint_Click(object sender, EventArgs e)
+        {
+            var startupPath = Application.StartupPath;
+            Application.Exit();
+            Helper.Send(new List<string> { "CMMProgram" });
+        }
+    }
+}
---
+++ EACT_Start/Helper.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace EACT_Start
+{
+    public abstract class Helper
+    {
+        public static void Send(List<string> args)
+        {
+            args.Insert(0, System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName);
+            string format = "\"{0}\"";
+            for (int i = 1; i < args.Count; i++)
+            {
+                format += " \"{" + i + "}\"";
+            }
+
+            string str = string.Format(format
+                , args.ToArray()
+                );
+
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.UseShellExecute = false;    //是否使用操作系统shell启动
+            p.StartInfo.RedirectStandardInput = true;//接受来自调用程序的输入信息
+            p.StartInfo.RedirectStandardOutput = true;//由调用程序获取输出信息
+            p.StartInfo.RedirectStandardError = true;//重定向标准错误输出
+            p.StartInfo.CreateNoWindow = true;//不显示程序窗口
+            p.Start();//启动程序
+
+            //向cmd窗口发送输入信息
+            p.StandardInput.WriteLine(str + "&exit");
+
+            p.StandardInput.AutoFlush = true;
+            //p.StandardInput.WriteLine("exit");
+            //向标准输入写入要执行的命令。这里使用&是批处理命令的符号，表示前面一个命令不管是否执行成功都执行后面(exit)命令，如果不执行exit命令，后面调用ReadToEnd()方法会假死
+            //同类的符号还有&&和||前者表示必须前一个命令执行成功才会执行后面的命令，后者表示必须前一个命令执行失败才会执行后面的命令
+            //获取cmd窗口的输出信息
+          
+            p.WaitForExit();//等待程序执行完退出进程
+            p.Close();
+        }
+
+        public static List<string> ExecBatCommand(Action<Action<string>> inputAction, Action<string> showMsg=null,bool isRead=true)
+        {
+            Process pro = null;
+            StreamWriter sIn = null;
+            StreamReader sOut = null;
+            List<string> outPutString = new List<string>();
+            try
+            {
+                pro = new Process();
+                pro.StartInfo.FileName = "cmd.exe";
+                pro.StartInfo.UseShellExecute = false;
+                pro.StartInfo.CreateNoWindow = true;
+                pro.StartInfo.RedirectStandardInput = true;
+                pro.StartInfo.RedirectStandardOutput = true;
+                pro.StartInfo.RedirectStandardError = true;
+
+                pro.OutputDataReceived += (sender, e) =>
+                {
+                    if (!string.IsNullOrEmpty(e.Data))
+                    {
+                        outPutString.Add(e.Data);
+                        if (showMsg != null)
+                        {
+                            showMsg(e.Data);
+                        }
+                    }
+                };
+
+                pro.ErrorDataReceived += (sender, e) =>
+                {
+                    if (!string.IsNullOrEmpty(e.Data))
+                    {
+                        outPutString.Add(e.Data);
+                        if (showMsg != null)
+                        {
+                            showMsg(e.Data);
+                        }
+                    }
+                };
+
+                pro.Exited += Pro_Exited;
+
+                pro.Start();
+                sIn = pro.StandardInput;
+                sIn.AutoFlush = true;
+                inputAction(value => sIn.WriteLine(value));
+                if (isRead)
+                {
+                    pro.BeginOutputReadLine();
+                    pro.BeginErrorReadLine();
+                }
+                pro.WaitForExit();
+            }
+            catch (Exception ex)
+            {
+                outPutString.Add(ex.Message);
+            }
+            finally
+            {
+                if (pro != null && !pro.HasExited)
+                    pro.Kill();
+
+                if (sIn != null)
+                    sIn.Close();
+                if (sOut != null)
+                    sOut.Close();
+                if (pro != null)
+                    pro.Close();
+            }
+
+            return outPutString;
+        }
+
+        private static void Pro_Exited(object sender, EventArgs e)
+        {
+            
+        }
+
+        public static string ExecBat(string batPath, ref string errMsg)
+        {
+            string outPutString = string.Empty;
+            using (Process pro = new Process())
+            {
+                FileInfo file = new FileInfo(batPath);
+                pro.StartInfo.WorkingDirectory = file.Directory.FullName;
+                pro.StartInfo.FileName = batPath;
+                pro.StartInfo.CreateNoWindow = false;
+                pro.StartInfo.RedirectStandardOutput = true;
+                pro.StartInfo.RedirectStandardError = true;
+                pro.StartInfo.UseShellExecute = false;
+
+                pro.Start();
+                pro.WaitForExit();
+
+                outPutString = pro.StandardOutput.ReadToEnd();
+                errMsg = pro.StandardError.ReadToEnd();
+            }
+            return outPutString;
+        }
+        
+    }
+}
