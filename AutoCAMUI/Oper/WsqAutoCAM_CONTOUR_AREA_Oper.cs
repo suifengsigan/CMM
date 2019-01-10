2d01ced9bba44cc08d713a433b78521ed0655002
suifengsigan <1075087594@qq.com>
2019-01-10 14:15:34 +0800

    优化

--- AutoCAMUI/Oper/WsqAutoCAM_CONTOUR_AREA_Oper.cs
+++ AutoCAMUI/Oper/WsqAutoCAM_CONTOUR_AREA_Oper.cs
@@ -5,12 +5,28 @@ using System.Text;
 
 namespace AutoCAMUI
 {
-    public class WsqAutoCAM_CONTOUR_AREA_Oper:CAMOper
+    /// <summary>
+    /// 曲面加工
+    /// </summary>
+    public class WsqAutoCAM_CONTOUR_AREA_Oper:WsqAutoCAM_Oper
     {
         public WsqAutoCAM_CONTOUR_AREA_Oper()
         {
-            AUTOCAM_TYPE = "WsqAutoCAM";
             AUTOCAM_SUBTYPE = "CONTOUR_AREA";
+        }
+
+        public override void SetCutDepth(double depth, int param_index = NXOpen.UF.UFConstants.UF_PARAM_CUTLEV_GLOBAL_CUT_DEPTH)
+        {
+            //base.SetCutDepth(depth, param_index);
+        }
+
+        /// <summary>
+        /// 设置加工区域
+        /// </summary>
+        /// <param name="ele">电极</param>
+        public void SetMillArea(ElecManage.Electrode ele)
+        {
+            Helper.SetCamgeom(NXOpen.UF.CamGeomType.CamCutArea, OperTag, Enumerable.Select(ele.ElecHeadFaces, u => u.NXOpenTag).ToList());
         }
     }
 }
